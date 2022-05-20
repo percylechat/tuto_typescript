0bcd471ec61463394a0b19162ec596bd7d4cb840
bussiere <bussiere@gmail.com>
2022-05-20 12:27:44 +0200

    wip master Updated: 2022-05-20 12:27:44

--- 42_auth/py42/42server.py
+++ 42_auth/py42/42server.py
@@ -1,9 +1,9 @@
-from typing_extensions import dataclass_transform
 from flask import Flask, request
 from flask import render_template
 import requests
 import json
 import secrets
+import urllib.parse
 
 
 app = Flask(__name__)
@@ -19,17 +19,35 @@ def hello():
     return render_template(
         "index.html",
         client_id=data_cred["client_id"],
-        redirect_uri=data_cred["redirect_uri"],
+        redirect_uri=urllib.parse.quote(data_cred["redirect_uri"], safe=""),
         secret_random=secret_random,
     )
 
 
 @app.route("/done/")
-def callback():
+def done():
     code = request.args.get("code", default="", type=str)
     print(code)
     if code != "toto":
         print(code)
+        data_42 = {
+            "grant_type": "authorization_code",
+            "client_id": data_cred["client_id"],
+            "client_secret": data_cred["client_id"],
+            "code": code,
+            "redirect_uri": data_cred["redirect_uri"],
+        }
+        r = requests.post(data_cred["url_42_token"], data=data_42)
+        print("result query token 42")
+        data_response_42_token = r.json()
+        print(data_response_42_token)
+        if (
+            "access_token" in data_response_42_token.keys()
+            and "error" not in data_response_42_token.keys()
+        ):
+            print("success loggued")
+        else:
+            print("failed loggin")
     return render_template("done.html")
 
 
@@ -43,6 +61,6 @@ def callback():
 
 
 if __name__ == "__main__":
-    with open("credential.json") as f:
+    with open("../credential.json") as f:
         data_cred = json.load(f)
     app.run()
--- 42_auth/py42/run.sh
+++ 42_auth/py42/run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 export FLASK_APP=42server
 export FLASK_ENV=development
-flask run
+python3 42server.py
